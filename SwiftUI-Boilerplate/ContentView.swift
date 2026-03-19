cab9cc757b5c42a33a3b4f38995548e154196791
John Patrick Echavez <johnpatrick.echavez@coreproc.com>
2026-03-19 20:15:17 +0800

    Initial Commit

---
+++ SwiftUI-Boilerplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Boilerplate
+//
+//  Created by John Patrick Echavez on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
